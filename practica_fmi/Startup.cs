2c0f73205cf58772e64ec5ab311bb9a30a34f6a9
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-03-31 19:58:03 +0300

    Add project files.

---
+++ practica_fmi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(practica_fmi.Startup))]
+namespace practica_fmi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
