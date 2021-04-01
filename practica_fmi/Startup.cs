9dfbfd84aa96ed1767b5f15bf6e4567b2e1f8f55
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-04-01 21:09:24 +0300

    add user roles

--- practica_fmi/Startup.cs
+++ practica_fmi/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using practica_fmi.Models;
 
 [assembly: OwinStartupAttribute(typeof(practica_fmi.Startup))]
 namespace practica_fmi
@@ -9,6 +12,71 @@ namespace practica_fmi
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            // create roles
+            CreateApplicationRoles();
+        }
+
+        private void CreateApplicationRoles()
+        {
+
+            ApplicationDbContext db = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            // Add application roles
+            if (!roleManager.RoleExists("Admin"))
+            {
+                // Add admin role
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+                // Add admin user
+                var admin = new ApplicationUser();
+                admin.UserName = "admin@gmail.com";
+                admin.Email = "admin@gmail.com";
+                var adminCreated = UserManager.Create(admin, "parolaadmin");
+
+                if (adminCreated.Succeeded)
+                {
+                    UserManager.AddToRole(admin.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Profesor"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Profesor";
+                roleManager.Create(role);
+
+                // Create dummy account for testing
+                var prof = new ApplicationUser();
+                prof.UserName = "prof1@gmail.com";
+                prof.Email = "prof1@gmail.com";
+                var userCreated = UserManager.Create(prof, "parolaprof1");
+
+                if (userCreated.Succeeded)
+                {
+                    UserManager.AddToRole(prof.Id, "Profesor");
+                }
+            }
+
+            if (!roleManager.RoleExists("Student"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Student";
+                roleManager.Create(role);
+
+                // Create dummy account for testing
+                var user = new ApplicationUser();
+                user.UserName = "std1@gmail.com";
+                user.Email = "std1@gmail.com";
+                var userCreated = UserManager.Create(user, "parolastudent1");
+
+                if (userCreated.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, "Student");
+                }
+            }
         }
     }
 }
