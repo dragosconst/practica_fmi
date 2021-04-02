48060291e77adf2f5434ce40c83345770e4141a1
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-04-02 21:08:38 +0300

    add CRUD students

--- practica_fmi/Models/Student.cs
+++ practica_fmi/Models/Student.cs
@@ -10,9 +10,17 @@ namespace practica_fmi.Models
     {
         [Key]
         public int StudentId { get; set; }
+        public string UserId { get; set; }
 
+        [Required(ErrorMessage = "Numele este obligatoriu")]
         public string Nume { get; set; }
+        [Required(ErrorMessage = "Prenumele este obligatoriu")]
         public string Prenume { get; set; }
+
+        [Required(ErrorMessage = "Emailul este obligatoriu")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Anul de studiu este obligatoriu")]
         public int AnStudiu { get; set; }
     }
 }
