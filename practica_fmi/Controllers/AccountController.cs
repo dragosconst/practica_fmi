d6883bb3e9cd70f3eecea44fb0c66760f3c0ed15
dragosconst <38582034+dragosconst@users.noreply.github.com>
2021-04-02 19:47:42 +0300

    add login by mail

--- practica_fmi/Controllers/AccountController.cs
+++ practica_fmi/Controllers/AccountController.cs
@@ -75,7 +75,9 @@ namespace practica_fmi.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            // Dragos edit: added user var to enable login by email (so email and username can be different)
+            var user = UserManager.FindByEmail(model.Email);
+            var result = await SignInManager.PasswordSignInAsync((user != null ? user.UserName : model.Email), model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
